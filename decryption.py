7bc0f8c96bd2c8804ef886d9a5f4dcfceb45a73a
JonaElezi <jona.elezi@student.uni-pr.edu>
2026-04-19 22:55:05 +0200

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -1,15 +1,15 @@
 from matrix import find_coords
 
-def decrypt (ciphertext, p_grid1, c_grid1, c_grid2, p_grid2):
+def decrypt(ciphertext, p_grid1, c_grid1, c_grid2, p_grid2):
     """Ciphertexti kthehet ne plaintext"""
 
-    ciphertext= ciphertext.upper() .replace("J", "I")
-    ciphertext = "" .join (filter(str.isalpha, ciphertext))
+    ciphertext = ciphertext.upper().replace("J", "I")
+    ciphertext = "".join(filter(str.isalpha, ciphertext))
 
     plaintext = ""
 
-    for i in range (0, len(ciphertext), 2):
-        char1, char2 = ciphertext[i], ciphertext[i+1]
+    for i in range(0, len(ciphertext), 2):
+        char1, char2 = ciphertext[i], ciphertext[i + 1]
 
         loc1 = find_coords(char1, c_grid1)
         loc2 = find_coords(char2, c_grid2)
@@ -21,4 +21,4 @@ def decrypt (ciphertext, p_grid1, c_grid1, c_grid2, p_grid2):
             plaintext += p_grid1[r1][c2]
             plaintext += p_grid2[r2][c1]
 
-            return plaintext
+    return plaintext
