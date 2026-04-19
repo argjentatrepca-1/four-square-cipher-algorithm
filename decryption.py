29e419c8fa57edce6147f79f6d794d86b6bc3f60
JonaElezi <jona.elezi@student.uni-pr.edu>
2026-04-19 13:18:43 +0200

    Update decryption.py
    lidhja e kodit me matricen

--- decryption.py
+++ decryption.py
@@ -1,4 +1,8 @@
+from matrix import find_coords
+
 def decrypt (ciphertext, p_grid1, c_grid1, c_grid2, p_grid2):
+    """Ciphertexti kthehet ne plaintext"""
+
     ciphertext= ciphertext.upper() .replace("J", "I")
     ciphertext = "" .join (filter(str.isalpha, ciphertext))
 
@@ -11,8 +15,8 @@ def decrypt (ciphertext, p_grid1, c_grid1, c_grid2, p_grid2):
         loc2 = find_coords(char2, c_grid2)
 
         if loc1 is not None and loc2 is not None:
-            r1, c1 = loc1[0], loc1[1]
-            r2, c2 = loc2[0], loc2[1]
+            r1, c1 = loc1
+            r2, c2 = loc2
 
             plaintext += p_grid1[r1][c2]
             plaintext += p_grid2[r2][c1]
